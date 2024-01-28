f84cbffe3c95a3337c8f14f861e291ea572555e8
jose osmar <osmar.ajr10@gmail.com>
2024-01-28 17:14:04 -0300

    Criados os cards e corrigido CSS da estrutura

---
+++ script.js
@@ -0,0 +1,6 @@
+const searchinput = document.getElementById('search-input');
+
+document.addEventListener('input', function(){
+    const searchTerm = searchinput.ariaValueMax.toLowerCase();
+    if (searchTerm === '')
+})
